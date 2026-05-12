86d2b89899f9bf39013ee1e69e62d11359939ac9
rafly1764 <reinsv1764@gmail.com>
2026-05-12 14:35:43 +0700

    Pre-Test

---
+++ Pre_Test/src/Main.java
@@ -0,0 +1,27 @@
+abstract class Karyawan {
+    protected String nama;
+    Karyawan(String nama) {
+        this.nama = nama;
+    }
+    abstract double hitungGaji();
+}
+
+class Programmer extends Karyawan {
+    private double gajiPokok;
+    Programmer(String nama, double gajiPokok) {
+        super(nama);
+        this.gajiPokok = gajiPokok;
+    }
+    @Override
+    double hitungGaji() {
+        return gajiPokok;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Programmer p1 = new Programmer("Rafly", 7000000);
+        System.out.println("Nama: " + p1.nama);
+        System.out.println("Total Gaji: Rp" + p1.hitungGaji());
+    }
+}
